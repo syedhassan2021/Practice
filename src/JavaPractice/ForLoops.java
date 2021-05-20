5c577187111859752aff5e823a79fe891b87f38b
Syed K Hassan <Syed K Hassan@DESKTOP-RNR5PHH>
2021-05-19 21:36:35 -0400

    add

---
+++ src/JavaPractice/ForLoops.java
@@ -0,0 +1,15 @@
+package JavaPractice;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		//
+		
+		for (int i=1; i<=10; i=i+2){
+			
+			System.out.println(i);
+		}
+		
+	}	
+		
+}
---
+++ src/JavaPractice/IfElse.java
@@ -0,0 +1,24 @@
+package JavaPractice;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	int joe =50;
+	int samantha =70;
+		
+		if (joe>samantha){  //To print first condition or 2nd condition whichever meets the criteria
+	System.out.println("Joe is elder than Samantha");
+	}
+		else{
+	System.out.println("Joe is younger than Samantha. His age is " + (joe));	 
+	}
+
+	
+	}
+			
+			
+	}	
+
+
